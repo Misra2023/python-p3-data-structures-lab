df3f17f1c7c8499f52f88103f7df63e1bdd613d1
misra2023 <mishazabdi@gmail.com>
2023-09-07 13:01:54 +0300

    completed assignment

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,38 +1,38 @@
-spicy_foods = [
-    {
-        "name": "Green Curry",
-        "cuisine": "Thai",
-        "heat_level": 9,
-    },
-    {
-        "name": "Buffalo Wings",
-        "cuisine": "American",
-        "heat_level": 3,
-    },
-    {
-        "name": "Mapo Tofu",
-        "cuisine": "Sichuan",
-        "heat_level": 6,
-    },
-]
+# data_structures.py
 
+# Task 1: get_names()
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
+# Task 2: get_spiciest_foods()
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
+# Task 3: print_spicy_foods()
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
 
+# Task 4: get_spicy_food_by_cuisine()
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
 
+# Task 5: print_spiciest_foods()
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    for food in spiciest_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
 
-def get_average_heat_level(spicy_foods):
-    pass
+# Task 6: average_heat_level()
+def average_heat_level(spicy_foods):
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    return total_heat_level // len(spicy_foods)
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+# Task 7: create_spicy_food()
+def create_spicy_food(spicy_foods, new_spicy_food):
+    spicy_foods.append(new_spicy_food)
+    return spicy_foods
